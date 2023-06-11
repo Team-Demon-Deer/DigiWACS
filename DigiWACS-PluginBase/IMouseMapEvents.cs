d60f2c1d0476b5c02788bce790117831d02952e0
TaeruAlethea <taerualethea@gmail.com>
2023-06-11 14:23:04 -0400

    IMouseMapEvents interface for composition structure

---
+++ DigiWACS-PluginBase/IMouseMapEvents.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiWACS.PluginBase {
+	internal interface IMouseMapEvents {
+
+		//Theorized Mouse Actions
+		void OnNoEntitySelect() { }
+		void OnSingleEntitySelect() { }
+		void OnMultiEntitySelect() { }
+	}
+}
