9c6b57d48f9566017cd3f73db4b526d050829dd6
bb00 <11166320+bb00@users.noreply.github.com>
2023-06-14 17:29:01 -0400

    update tracks rendering

--- DigiWACS-Client/UnitTracksProvider.cs
+++ DigiWACS-Client/UnitTracksProvider.cs
@@ -27,49 +27,68 @@ namespace DigiWACS.Client;
 internal sealed class UnitTracksProvider : MemoryProvider, IDynamic, IDisposable
 {
     public event DataChangedEventHandler? DataChanged;
-    private ConcurrentBag<IFeature> features = new ConcurrentBag<IFeature>();
-    private readonly object balanceLock = new object();  
+    private ConcurrentDictionary<int,IFeature> features = new ConcurrentDictionary<int, IFeature>();
+    private ConcurrentDictionary<int,IFeature> renderFeatures = new ConcurrentDictionary<int, IFeature>();
+    private readonly object dataLock = new object();
+    private readonly object renderLock = new object();
     private readonly PeriodicTimer _timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
-    private readonly PeriodicTimer _refrsh = new PeriodicTimer(TimeSpan.FromSeconds(5));
-
+    private readonly PeriodicTimer _refrsh = new PeriodicTimer(TimeSpan.FromSeconds(2));
+    private readonly PeriodicTimer _tick = new PeriodicTimer(TimeSpan.FromSeconds(0.05));
     public UnitTracksProvider() {
-        Catch.TaskRun(RunTimerAsync);
-
+            Catch.TaskRun(RunTimerAsync);
 	    var thread = new Thread(new ThreadStart(this.RefreshData));
 	    thread.IsBackground = true;
 	    thread.Start();
+	    var thread2 = new Thread(new ThreadStart(this.RenderTracks));
+	    thread2.IsBackground = true;
+	    thread2.Start();
     }
 
     private (double Lon, double Lat) _prevCoords = (37.359, 45.006);
 
     private void RefreshData() {
         while(true) {
+    	_timer.WaitForNextTickAsync().Wait();
         var input = new QueryRequest { Statement = "SELECT id, st_x(st_transform(POSITION::geometry, 4326)) AS long, st_y(st_transform(POSITION::geometry, 4326)) AS lat, altitude, TYPE, NAME, callsign, player, group_name, coalition, heading, speed, updated_at FROM PUBLIC.units WHERE coalition = 3" };
-        using (var channel = GrpcChannel.ForAddress("http://192.168.90.1:50051")) {
-            var client = new Postgres.PostgresClient(channel);
-            using (var reply = client.Query(input)) {
-	            Console.WriteLine("Connected");
-                var rows = reply.ResponseStream.ReadAllAsync().ToBlockingEnumerable();
-	   	        Console.WriteLine("test");
-	   	        foreach (var row in rows) {
-	   	           PointFeature a = new PointFeature(SphericalMercator.FromLonLat(row.Fields["long"].NumberValue, row.Fields["lat"].NumberValue).ToMPoint());
-	   	           a["ID"] = row.Fields["id"].NumberValue;
-	   	           a["name"] = row.Fields["name"].StringValue;
-	   	           a["rotation"] = row.Fields["heading"].NumberValue - 180;
-	   	           lock (balanceLock)
-	   	           {
-	   	               this.features.Add(a);
-	   	           }
-	   	        }
+            using (var channel = GrpcChannel.ForAddress("http://192.168.90.1:50051")) {
+                var client = new Postgres.PostgresClient(channel);
+                using (var reply = client.Query(input)) {
+	                Console.WriteLine("Connected");
+                    var rows = reply.ResponseStream.ReadAllAsync().ToBlockingEnumerable();
+	   	            Console.WriteLine("test");
+	   	            foreach (var row in rows) {
+    	   	            PointFeature a = new PointFeature(SphericalMercator.FromLonLat(row.Fields["long"].NumberValue, row.Fields["lat"].NumberValue).ToMPoint());
+	       	            a["ID"] = row.Fields["id"].NumberValue;
+	   	                a["name"] = row.Fields["name"].StringValue;
+	   	                a["rotation"] = row.Fields["heading"].NumberValue - 180;
+	   	                lock (dataLock) {
+    	   	                lock (renderLock) {
+	   	                        this.features[(int)row.Fields["id"].NumberValue] = a;
+    	   	                    this.renderFeatures[(int)row.Fields["id"].NumberValue] = new PointFeature(a);
+	   	                    }
+	       	            }
+	   	            }
+                }
             }
         }
-        }
     }
+    private void RefreshTracks() {
+        while(true) {
+            Task.Delay()
+           foreach (var track in renderFeatures) {
+               lock (renderLock) {
+                    track.CoordinateVisitor((x, y, func) => {    
+                        var (xO, yO) = ((i,j) => {return (i+343*0.05, j);})(x,y);
+                        func(xO, yO);
+                    });
+               }
+           }
+        } 
+    } 
     
     private async Task RunTimerAsync()
     {
         while (true) {
-
             await _timer.WaitForNextTickAsync();
                        OnDataChanged();
         }
@@ -86,7 +105,7 @@ internal sealed class UnitTracksProvider : MemoryProvider, IDynamic, IDisposable
     }
 
     public override Task<IEnumerable<IFeature>> GetFeaturesAsync(FetchInfo fetchInfo) {  
-            return Task.FromResult((IEnumerable<IFeature>)features);
+            return Task.FromResult((IEnumerable<IFeature>)renderFeatures);
 
         //var input = new SingleUnitPositionRequest { Id = "1000263"};
         //var channel = GrpcChannel.ForAddress("http://sonic.local:8001");
