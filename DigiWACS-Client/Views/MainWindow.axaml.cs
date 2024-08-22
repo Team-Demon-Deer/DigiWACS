690bfb0e8f5335df6ab0a8059f094b05445eddd3
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-08-22 19:14:48 -0400

    Incorporated Avalonia.UpDock

--- DigiWACS-Client/Views/MainWindow.axaml.cs
+++ DigiWACS-Client/Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Layout;
 
 namespace DigiWACS_Client.Views;
 
@@ -7,5 +11,50 @@ public partial class MainWindow : Window
 	public MainWindow()
 	{
 		InitializeComponent();
+	}
+	
+	
+	private void UnclosableTab_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+	{
+		e.Cancel = true;
+
+		var popup = new MessagePopup("You can't close this tab.\nMaybe there's unsaved work or something idk")
+		{
+			WindowStartupLocation = WindowStartupLocation.CenterOwner
+		};
+		popup.ShowDialog(this);
+	}
+
+	protected override void OnLoaded(RoutedEventArgs e)
+	{
+		base.OnLoaded(e);
+		if (Design.IsDesignMode)
+			return;
+	}
+}
+
+public class MessagePopup : Window
+{
+	public MessagePopup(string message)
+	{
+		SizeToContent = SizeToContent.WidthAndHeight;
+
+		var panel = new StackPanel() { Spacing = 10, Margin = new Thickness(10) };
+
+		var okButton = new Button() { Content = "OK", HorizontalAlignment = HorizontalAlignment.Right };
+
+		okButton.Click += (s, e) => Close();
+
+		panel.Children.Add(new TextBlock() { Text = message});
+		panel.Children.Add(okButton);
+
+		Content = panel;
+	}
+
+	protected override void OnKeyDown(KeyEventArgs e)
+	{
+		base.OnKeyDown(e);
+		if (e.Key == Key.Enter)
+			Close();
 	}
 }
