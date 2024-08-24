dda95b32336b364ef394217797ebea81082af0b8
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-08-23 23:39:47 -0400

    Prep Primary and Secondary Hook

--- DigiWACS-Client/ViewModels/MainWindowViewModel.cs
+++ DigiWACS-Client/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Mapsui.UI.Avalonia;
+using NetTopologySuite.Geometries;
 using ReactiveUI;
 
 namespace DigiWACS_Client.ViewModels;
@@ -7,6 +8,7 @@ namespace DigiWACS_Client.ViewModels;
 public partial class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
+	
 	public string Greeting => "Welcome to Avalonia!";
 
 	private MapControl areaMap;
@@ -17,5 +19,20 @@ public partial class MainWindowViewModel : ViewModelBase
 		set => areaMap = value;
 	}
 
+	private Coordinate hookPrimary;
+	private Coordinate hookSecondary;
+
+	public Coordinate HookPrimary
+	{
+		get => hookPrimary;
+		set => hookPrimary = value;
+	}
+	
+	public Coordinate HookSecondary
+	{
+		get => hookSecondary;
+		set => hookSecondary = value;
+	}
+	
 #pragma warning restore CA1822 // Mark members as static
 }
