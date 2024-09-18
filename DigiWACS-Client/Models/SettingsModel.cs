61a8a24146df43b411bd587a6fcdb4dd1902e11c
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2024-09-18 15:43:42 -0400

    Remove old example stuff

--- DigiWACS-Client/Models/SettingsModel.cs
+++ DigiWACS-Client/Models/SettingsModel.cs
@@ -4,7 +4,7 @@ public sealed class SettingsModel {
 	public required int KeyOne { get; set; }
 	public required bool KeyTwo { get; set; }
 	public required NestedSettings KeyThree { get; set; } = null!;
-	public required string[] IPAddressRange { get; set; }
+	public required ServerConnections[] ServerConnections { get; set; }
 }
 
 public sealed class NestedSettings {
